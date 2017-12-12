6b86613ec4f6f831d1942413edfa7814c7d429c9
mohamadem60mdem <mohamadem60@yahoo.com>
2017-12-11 19:37:19 -0800

    app2

---
+++ app/app2.js
@@ -0,0 +1,32 @@
+/*
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+  
+  const myAwesomeDB = database.db('mydbXXX');
+  //  myAwesomeDB.collection('theCollectionIwantToAccess'){ 
+
+    myAwesomeDB.createCollection("CollectionXXX", function(err, res) {
+    if (err) throw err;
+    console.log("Collection and DB created!");
+    database.close();
+  });
+});
+*/
+//a2
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+
+  const myAwesomeDB = database.db('mydb4');
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  myAwesomeDB.collection("collection1").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    database.close();
+  });
+});
