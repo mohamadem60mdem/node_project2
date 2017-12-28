76665499aec68dc2fa58637babb9d87c189f8e5b
mohamadem60mdem <mohamadem60@yahoo.com>
2017-12-28 01:59:51 -0800

    f
    f

---
+++ app/app10.js
@@ -0,0 +1,7 @@
+// Create Files with The fs.appendFile() 
+var fs = require('fs');
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
---
+++ app/app11.js
@@ -0,0 +1,8 @@
+//Create a new, empty file using the open() method:
+
+var fs = require('fs');
+
+fs.open('mynewfile2.txt', 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Saved!');
+});
